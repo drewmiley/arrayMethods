7d5c78181fdb94ec5bb98532a9ef649d30c13ae9
Drew Miley <drew.miley@opencastsoftware.com>
2019-03-11 15:45:17 +0000

    Some Work on FilterFind

--- src/main/scala/FilterFind.scala
+++ src/main/scala/FilterFind.scala
@@ -1,20 +1,24 @@
+import helper.{ExampleLists, IVO}
+
 import scala.concurrent.ExecutionContext
 
 object FilterFind extends App {
   implicit val ec = ExecutionContext.global
 
+  val exampleListNoOption: List[IVO] = ExampleLists.noOptions
+  val filterNoOption: List[IVO] = exampleListNoOption filter {ivo => ivo.number > 0}
+  val filterNotNoOption: List[IVO] = exampleListNoOption filterNot {ivo => ivo.number > 0}
+  val findNoOption: Option[IVO] = exampleListNoOption find {ivo => ivo.number > 0}
+  val forallNoOption: Boolean = exampleListNoOption forall {ivo => ivo.number > 0}
+  val existsNoOption: Boolean = exampleListNoOption exists {ivo => ivo.number > 0}
+  val countNoOption: Int = exampleListNoOption count {ivo => ivo.number > 0}
+  val containsNoOption: Boolean = exampleListNoOption contains {IVO()}
+
+  val exampleListOptionProperties: List[IVO] = ExampleLists.optionProperties
+
+  val exampleListOptionObject: List[Option[IVO]] = ExampleLists.optionObject
+
+  val exampleListOptionObjectAndProperties: List[Option[IVO]] = ExampleLists.optionObjectAndProperties
+
   println("FilterFind")
-
-  //  val exampleList: List[helper.IVO] = ExampleLists.noOptions
-  //  val exampleList: List[helper.IVO] = ExampleLists.optionProperties
-  //  val exampleList: List[Option[helper.IVO]] = ExampleLists.optionObject
-  //  val exampleList: List[Option[helper.IVO]] = ExampleLists.optionObjectAndProperties
-
-  //  val dfdsf = exampleList.filter()
-  //  val sdfsd = exampleList.filterNot()
-  //  val sdfds = exampleList.find()
-  //  val dfsd = exampleList.forall()
-  //  val dsdfsd = exampleList.contains()
-  //  val sdfsd = exampleList.count()
-  //  val ddsf = exampleList.exists()
 }
