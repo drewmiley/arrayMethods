50c7a494ea91e96b15fd8e5a75f6f6cfe7df2b6f
Drew Miley <drew.miley@opencastsoftware.com>
2019-03-11 15:50:46 +0000

    Some work on Index

--- src/main/scala/Index.scala
+++ src/main/scala/Index.scala
@@ -6,17 +6,16 @@ object Index extends App {
   implicit val ec = ExecutionContext.global
 
   val exampleListNoOption: List[IVO] = ExampleLists.noOptions
+  val indexOfNoOption: Int = exampleListNoOption indexOf {ExampleLists.noOptions(2)}
+  val lastIndexOfNoOption: Int = exampleListNoOption lastIndexOf {ExampleLists.noOptions(3)}
+  val indexWhereNoOption: Int = exampleListNoOption indexWhere {ivo => ivo.number > 0}
+  val lastIndexWhereNoOption: Int = exampleListNoOption lastIndexWhere {ivo => ivo.number > 0}
 
   val exampleListOptionProperties: List[IVO] = ExampleLists.optionProperties
 
   val exampleListOptionObject: List[Option[IVO]] = ExampleLists.optionObject
 
   val exampleListOptionObjectAndProperties: List[Option[IVO]] = ExampleLists.optionObjectAndProperties
-
-  //  val edsdsf = exampleList.indexOf()
-  //  val sdfdsf = exampleList.indexWhere()
-  //  val sdfsd = exampleList.lastIndexOf()
-  //  val dsfs = exampleList.lastIndexWhere()
 
   println("Index")
 }
