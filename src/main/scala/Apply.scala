34d087abeb6f2b0c38cd86f80c1d9d79b7f9a38a
Drew Miley <drew.miley@opencastsoftware.com>
2019-03-11 12:57:12 +0000

    Work on apply

--- src/main/scala/Apply.scala
+++ src/main/scala/Apply.scala
@@ -1,16 +1,25 @@
+import helper.{ExampleLists, IVO}
+
 import scala.concurrent.ExecutionContext
 
 object Apply extends App {
   implicit val ec = ExecutionContext.global
+  // applyOrElse()
+
+  val exampleListNoOption: List[IVO] = ExampleLists.noOptions
+  val applyNoOption = exampleListNoOption.apply(1)
+
+  val exampleListOptionProperties: List[IVO] = ExampleLists.optionProperties
+  val applyOptionProperties = exampleListOptionProperties.apply(1)
+
+  val exampleListOptionObject: List[Option[IVO]] = ExampleLists.optionObject
+  val applyOptionObject = exampleListOptionObject.apply(1)
+
+  val exampleListOptionObjectAndProperties: List[Option[IVO]] = ExampleLists.optionObjectAndProperties
+  val applyOptionObjectAndProperties = exampleListOptionObjectAndProperties.apply(1)
+
+  val noneList: Option[List[IVO]] = None
+  val orElse = noneList.orElse(Some(List(IVO)))
 
   println("Apply")
-
-  //  val exampleList: List[helper.IVO] = ExampleLists.noOptions
-  //  val exampleList: List[helper.IVO] = ExampleLists.optionProperties
-  //  val exampleList: List[Option[helper.IVO]] = ExampleLists.optionObject
-  //  val exampleList: List[Option[helper.IVO]] = ExampleLists.optionObjectAndProperties
-
-  //  val dsfdsf = exampleList.apply()
-  //  val sdfdsf = exampleList.applyOrElse()
-  //  val dfsdf = exampleList.orElse()
 }
